218f3ace1f77608445888a417015049d25f1548b
ardymarky <aqmarkin@gmail.com>
2024-09-30 18:57:25 +0000

    everything looks good

--- relay_boot.py
+++ relay_boot.py
@@ -1,13 +1,21 @@
 import RPi.GPIO as GPIO
 import time
 import subprocess
+import datetime
+from serial import Serial
+from pyubx2 import UBXReader
 
 # Set up GPIO using BCM numbering
 GPIO.setmode(GPIO.BCM)
 
 # Set up GPIO pin as an input
-relay_pin = 16  # Change this to match your wiring
+relay_pin = 16
 GPIO.setup(relay_pin, GPIO.IN)
+
+# Default port and baud
+ins_port = '/dev/ttyAMA0'
+ins_baud = 38400
+time_init = False
 
 # Variable to track the state of the relay
 relay_state = GPIO.input(relay_pin)
@@ -43,6 +51,38 @@ def ping_ip(ip_address):
     result = subprocess.run(['ping', '-c', '1', ip_address], stdout=subprocess.PIPE)
     return result.returncode == 0
 
+# Calculate UNIX time given GPS time
+def calc_posix_time (gps_tow, gps_week, gps_leapS):
+    return gps_tow/1000 + int(gps_week) * 604800 + 315964800 - gps_leapS
+
+# Open serial port to Hadron 640-r  
+stream = Serial(ins_port, ins_baud, timeout=3)
+# Clear buffer
+stream.reset_input_buffer()
+
+
+ubr = UBXReader(stream, protfilter=2)
+(raw_data, parsed_data) = ubr.read()
+
+while not time_init:
+    # Only sync if data is GPS Time
+    if parsed_data.identity == "NAV-TIMEGPS":
+        print("gps connected!")
+        
+        # Convert GPS time to UNIX time
+        cur_posix_time = calc_posix_time(parsed_data.iTOW, parsed_data.week, parsed_data.leapS)-18000
+        
+        # Set System Time
+        formatted_time = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(cur_posix_time))
+        command = ['sudo', 'date', '--set', formatted_time]
+        subprocess.run(command, check=True)
+        #time.clock_settime(time.CLOCK_REALTIME, cur_posix_time)
+        time_init = True
+
+    time.sleep (0.001) # Limit how fast the loop runs
+    (raw_data, parsed_data) = ubr.read()
+
+    
 while ping_ip("192.168.26.26") == 0:
     print("waiting for LAN connection")
     
